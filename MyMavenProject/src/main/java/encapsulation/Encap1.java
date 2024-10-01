ee3ae6edaef1bd0a1de83c17d2bb1d46c2d05235
USER <USER@LAPTOP-K54LAQ86>
2024-10-01 10:56:45 +0530

    First commit

---
+++ MyMavenProject/src/main/java/encapsulation/Encap1.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class Encap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encap obj=new Encap();
+obj.setName("haii");
+obj.setAge(5);
+System.out.println(obj.getName());
+System.out.println(obj.getAge());
+	}
+
+}
