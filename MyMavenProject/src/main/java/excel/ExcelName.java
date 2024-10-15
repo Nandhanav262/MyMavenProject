61f29525129cd2214563b396af05fb4e967e94d6
USER <USER@LAPTOP-K54LAQ86>
2024-10-15 10:54:23 +0530

    8th commit

--- MyMavenProject/src/main/java/excel/ExcelName.java
+++ MyMavenProject/src/main/java/excel/ExcelName.java
@@ -1,6 +1,11 @@
 package excel;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelName {
+static XSSFWorkbook w;
+static XSSFSheet sh;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- MyMavenProject/src/main/java/excel/ExcelRead.java
+++ MyMavenProject/src/main/java/excel/ExcelRead.java
@@ -1,10 +1,33 @@
 package excel;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Cell;
+
 public class ExcelRead {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static FileInputStream f;
+	static XSSFWorkbook w;
+	static XSSFSheet sh;
+	
+	public static String get_string_data(int a,int b) throws IOException
+	{
+		f= new FileInputStream("C:\\Users\\suhai\\OneDrive\\Desktop\\sumayya\\ExcelRead.xlsx");
+		w= new XSSFWorkbook (f);
+		sh=w.getSheet("Sheet1");
+		Row r=sh.getRow(a);
+		Cell c=r.getCell(b);
+		return c.getStringCellValue();
+		}
+	
+	public static void get_integer_data()
+	{
+		
 	}
+
+	
 
 }
