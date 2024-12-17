0bdfe09dca5043e65c1f429cd04858ce441a12ef
USER <USER@LAPTOP-K54LAQ86>
2024-12-17 09:12:43 +0530

    new coomit

---
+++ MyMavenProject/src/main/java/variable/Sample.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Sample {
+	int c=2;
+	static int b=5;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+System.out.println(a);
+System.out.println(b);
+
+Sample obj=new Sample();
+System.out.println(obj.c);
+	}
+
+}
